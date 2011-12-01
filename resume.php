f03700ce89578e1538b2212e3ea871abdadce767
Soo-Hyun Yoo <yoos117@gmail.com>
2011-12-01 00:13:40 -0800

    Add resume page.

---
+++ resume.php
@@ -0,0 +1,186 @@
+<html>
+<head>
+
+    <title>Soo-Hyun Yoo | Programmer, Project Manager | yoos117@gmail.com</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> 
+    <link rel="stylesheet" type="text/css" href="css/resume.css" media="all" />
+
+</head>
+<body>
+
+<div id="doc2" class="yui-t7">
+    <div id="inner">
+    
+        <div id="hd">
+            <div class="yui-gc">
+                <div class="yui-u first">
+                    <h1>Soo-Hyun Yoo</h1>
+                    <h2>Programmer, Project Manager</h2>
+                </div>
+
+                <div class="yui-u">
+                    <div class="contact-info">
+                        <h3><a id="pdf" href="yoos_resume.pdf">Download PDF</a></h3>
+                        <h3><a href="mailto:yoos117@gmail.com">yoos117@gmail.com</a></h3>
+                        <h3>(740) 343-9667</h3>
+                    </div><!--// .contact-info -->
+                </div>
+            </div><!--// .yui-gc -->
+        </div><!--// hd -->
+
+        <div id="bd">
+            <div id="yui-main">
+                <div class="yui-b">
+
+                    <div class="yui-gf">
+                        <div class="yui-u first">
+                            <h2>Profile</h2>
+                        </div>
+                        <div class="yui-u">
+                            <p class="enlarge">
+                                I am passionate about what I do. Persistent and adaptable, I can live with uncertainty and am always open to change. I procrastinate a bit, but under pressure, I am the best.
+                            </p>
+                        </div>
+                    </div><!--// .yui-gf -->
+
+                    <div class="yui-gf">
+                        <div class="yui-u first">
+                            <h2>Skills</h2>
+                        </div>
+                        <div class="yui-u">
+                            <div class="talent">
+                                <h2>Problem-solving</h2>
+                                <p>Attack challenges with insight, resourcefulness, and force.</p>
+                            </div>
+
+                            <div class="talent">
+                                <h2>Programming</h2>
+                                <p>Effective, efficient, and beautiful code, in that order.</p>
+                            </div>
+
+                            <div class="talent">
+                                <h2>Project Management</h2>
+                                <p>Proven ability to lead and unify individuals and teams.</p>
+                            </div>
+                        </div>
+                    </div><!--// .yui-gf -->
+
+                    <div class="yui-gf">
+                        <div class="yui-u first">
+                            <h2>Technical</h2>
+                        </div>
+                        <div class="yui-u">
+                            <ul class="talent">
+                                <li>Python</li>
+                                <li>C++</li>
+                                <li>Lisp</li>
+                                <li>HTML/CSS</li>
+                                <li class="last">PHP/MySQL</li>
+                            </ul>
+
+                            <ul class="talent">
+                                <li>SolidWorks</li>
+                                <li>Git/Subversion</li>
+                                <li class="last">KiCAD (PCB layout)</li>
+                            </ul>
+
+                            <ul class="talent">
+                                <li>Linux</li>
+                                <li>Windows</li>
+                                <li class="last">Mac OS</li>
+                            </ul>
+                        </div>
+                    </div><!--// .yui-gf-->
+
+                    <div class="yui-gf">
+    
+                        <div class="yui-u first">
+                            <h2>Experience</h2>
+                        </div><!--// .yui-u -->
+
+                        <div class="yui-u">
+                            <div class="job">
+                                <h2>OSU Dynamic Robotics Lab</h2>
+                                <h3>Programmer</h3>
+                                <h4>September 2011 &mdash; Present</h4>
+                                <p>Program software systems for ATRIAS, a bipedal running robot, using C and C++. Write code for realtime communication between software controller running inside the Robot Operating System (ROS) framework and EtherCAT slaves using shared memory in kernel space.</p>
+                            </div>
+
+                            <div class="job">
+                                <h2>The Corvallis Clinic</h2>
+                                <h3>Data Analyst</h3>
+                                <h4>June 2011 &mdash; Present</h4>
+                                <p>Research resource expenditures of The Corvallis Clinic and analyze energy savings achieved by their new Electronic Records System, HVAC system upgrades, and lighting upgrades. Help organize findings to include in Community Impact Report to present to the Sustainability Coalition. Write programs to parse the OBME database according to various filters to forecast Clinic recruiting needs for the next five years.</p>
+                            </div>
+
+                            <div class="job">
+                                <h2>FIRST Robotics Team 955</h2>
+                                <h3>Co-Program Manager</h3>
+                                <h4>November 2010 &mdash; May 2011</h4>
+                                <p>Manage entire FIRST Robotics program at Crescent Valley High School with focus on the team's technical operations. Accountable for team morale, budget, activities, human resources, and build schedule for a team of 40 students and 10 mentors. Supervise multiple projects in parallel, allocating resources as needed.</p>
+                            </div>
+
+                            <div class="job">
+                                <h2>FIRST Robotics Team 955</h2>
+                                <h3>Programmer</h3>
+                                <h4>September 2009 &mdash; October 2010</h4>
+                                <p>Program teleoperated/autonomous robot using C++ in WindRiver. Use webcam for machine vision. Maintain team web server. Manage team code with Subversion.</p>
+                            </div>
+
+                            <div class="job last">
+                                <h2>OSU Dynamic Robotics Lab</h2>
+                                <h3>Programming Intern</h3>
+                                <h4>June 2010 &mdash; August 2010</h4>
+                                <p>Use C++, GTK+, and Qt to program GUIs.
+                                Use CMake for project management.
+                                Explore the Robot Operating System (ROS) software framework.
+                                Use Blender for 3D modeling.
+                                Use Panda3D for robot visualization.</p>
+                            </div>
+
+                        </div><!--// .yui-u -->
+                    </div><!--// .yui-gf -->
+
+
+                    <div class="yui-gf last">
+                        <div class="yui-u first">
+                            <h2>Education</h2>
+                        </div>
+                        <div class="yui-u">
+                            <h2>Crescent Valley High School</h2>
+                            <h3>AP: Chemistry, Physics C Mech, Calculus, U.S. Government, French, Computer Science A &mdash; <strong>3.92 GPA</strong> </h3>
+                        </div>
+                    </div><!--// .yui-gf -->
+
+                    <!--<div class="yui-gf last">
+                        <div class="yui-u first">
+                            <h2>References</h2>
+                        </div>
+                        <div class="yui-u">
+                        </div>
+                    </div>-->
+
+                </div><!--// .yui-b -->
+            </div><!--// yui-main -->
+        </div><!--// bd -->
+
+        <div id="ft">
+            <p>Want to see even more? <a href="yoos_resume.pdf">Download the PDF</a>.</p>
+            <br />
+            <p>Soo-Hyun Yoo &mdash; <a href="mailto:yoos117@gmail.com">yoos117@gmail.com</a> &mdash; (740) 343-9667</p>
+        </div><!--// footer -->
+
+    </div><!-- // inner -->
+
+</div><!--// doc -->
+
+<?php include_once("ga.php") ?>
+
+</body>
+</html>
+
