489e199b410a5a97a9e138881e91f595bb543b7a
Soo-Hyun Yoo <yoos117@gmail.com>
2011-12-01 00:13:28 -0800

    Add index.php.

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+<title>iterativecode</title>
+
+<style type="text/css">
+body {
+    margin: 0;
+    text-shadow: 2px 2px 2px #ccc;
+    }
+a {
+    color: #cc1122;
+    }
+.block {
+    position: absolute;
+    width: 500px;
+    padding-left: 10px;
+    border-left: solid 1px #666;
+    margin-left: -250px;
+    margin-top: -200px;
+    left: 50%;
+    top: 60%;
+    }
+#footer {
+    position: absolute;
+    width: 500px;
+    margin-left: -250px;
+    left: 50%;
+    bottom: 20px;
+    font-size: 10px;
+    font-family: Arial;
+    color: #999;
+    letter-spacing: 2em;
+    text-align: center;
+    }
+</style>
+
+</head>
+
+<body>
+
+<div class="block">
+<p style="font-size: 24px; margin: 0;">Hi, I'm Soo-Hyun.</p>
+
+<p>I am a 
+    <a href="/blog/category/programming/">programmer</a> and
+    <a href="/blog/category/projects/">hacker</a>.
+    I am a former Co-Program Manager for <a href="http://cvhsrobotics.net/">FIRST Robotics Team 955</a>.
+    I am a Linux enthusiast,
+    a <a href="/blog/2011/03/07/corvallis-camerata-wins-1st-place/">violinist</a>, and
+    a Lincoln-Douglas debater.</p>
+
+<p>I want to found tech startups in Silicon Valley.</p>
+
+<p>I like to play tennis and would like to overcome my fear of the spiders that crawl into my room through the floor vent.
+
+<p>I haven't decided what I want to do with this page, so I put this together in 5 minutes. In the meantime, <a href="/blog">read my blog</a> to get to know me better!</p>
+</div>
+
+<div id="footer">
+iterativecode
+</div>
+
+<?php include_once("ga.php") ?>
+
+</body>
+
+</html>
