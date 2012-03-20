111730b23adcd2d38c191fdc80b2b6cf5ac492eb
Soo-Hyun Yoo <yoos117@gmail.com>
2012-03-20 01:56:32 -0700

    Remove static sidebar items.

--- blog/wp-content/themes/minimalist/sidebar.php
+++ blog/wp-content/themes/minimalist/sidebar.php
@@ -1,68 +1,35 @@
 <div id="leftcol" >
 <div id="menu">
 
-<h2 class="menuheader">Pages</h2>
-<div class="menucontent">
 <ul>
-<?php wp_list_pages('sort_column=menu_order&title_li='); ?>
+	<?php if (!dynamic_sidebar()) : ?>
+	<li><?php get_search_form(); ?></li>
 </ul>
-</div>
 
-<h2 class="menuheader">Categories</h2>
-<div class="menucontent">
-<ul>
-<?php wp_list_categories('title_li=&show_count=0'); ?>
+<ul role="navigation">
+	<?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
+	<li><h2>Archives</h2>
+		<ul>
+		<?php wp_get_archives(array('type' => 'monthly')); ?>
+		</ul>
+	</li>
+
+	<?php wp_list_categories(array('show_count' =>1, 'title_li' => '<h2>Categories</h2>')); ?>
 </ul>
-</div>
-
-
-<h2 class="menuheader">Archives</h2>
-<div class="menucontent">
-<ul>
-<?php wp_get_archives('type=monthly'); ?>
-</ul>
-</div>
-
-
-<h2 class="menuheader">Links</h2>
-<div class="menucontent">
-<ul>
-<?php wp_list_bookmarks('title_li=&categorize=0'); ?>
-</ul>
-</div>
-
-<h2 class="menuheader">Customise</h2>
-<div class="menucontent">
 
 <ul>
-<li><a href="#" onclick="setActiveStyleSheet('white'); return false;">White</a></li>
-<li><a href="#" onclick="setActiveStyleSheet('black'); return false;">Black</a></li>
-<li><a href="#" onclick="setActiveStyleSheet('blue'); return false;">Blue</a></li>
-<li><a href="#" onclick="setActiveStyleSheet('green'); return false;">Green</a></li>
-<li><a href="#" onclick="setActiveStyleSheet('grey'); return false;">Grey</a></li>
+	<?php if (is_home() || is_page()) {   /* Is this the front page? */ ?>
+		<?php wp_list_bookmarks(); ?>
+
+		<li><h2>Meta</h2>
+		<ul>
+			<?php wp_register(); ?>
+			<li><?php wp_loginout(); ?></li>
+			<?php wp_meta(); ?>
+		</ul></li>
+	<?php } endif; ?>
 </ul>
 
 </div>
-
-
-
-<h2 class="menuheader">Info</h2>
-<div class="menucontent">
-<ul>
-<?php wp_register(); ?>
-<li><?php wp_loginout(); ?></li>
-<li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-<li>Design by <a href="http://www.techdesigns.co.uk" title="Design by Techdesigns">Techdesigns</a></li>
-<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">Powered by WordPress</a></li>
-<?php wp_meta(); ?>
-</ul>
 </div>
 
-<?php if ( !function_exists('dynamic_sidebar')         || !dynamic_sidebar() ); ?>
-
-
-
-
-</div>
-</div>
