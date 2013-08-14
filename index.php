49a1725485c3ebd6ce0333dcb8107ae5734e3d8f
Soo-Hyun Yoo <yoos@betelgeuse.(none)>
2013-08-13 17:26:48 -0700

    Update index.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ body {
     color: #666;
     font-family: Arial;
     margin: 0;
-    text-shadow: 1px 1px 1px #ccc;
+    text-shadow: 0px 0px 1px #ccc;
     }
 a {
     color: #000000;
@@ -16,13 +16,13 @@ a {
     }
 .block {
     position: absolute;
-    width: 500px;
+    width: 400px;
     padding-left: 10px;
     border-left: solid 1px #666;
-    margin-left: -250px;
+    margin-left: -200px;
     margin-top: -200px;
     left: 50%;
-    top: 60%;
+    top: 50%;
     }
 #footer {
     position: absolute;
@@ -48,19 +48,25 @@ a {
 <p>Some of the things I've worked on:</p>
 
 <ul>
-    <li><a href="/blog/tag/kiwi">Kiwi drive</a></li>
+    <li><a href="/blog/2012/02/27/kiwi-drive-videos/">Kiwi drive <a href="/blog/2012/04/18/l298n-dual-full-bridge-motor-driver/">robot</a></li>
     <li><a href="/blog/2011/07/20/ceramic-hand">Ceramics</a></li>
-    <li><a href="/blog/tag/pcb">Cupric chloride PCB etching</a></li>
+    <li><a href="/blog/2011/06/27/diy-pcb-etching-overview-and-production-of-ferric-chloride/">DIY</a> <a href="/blog/2011/07/02/diy-pcb-etching-electrolyzing-salt-to-produce-cuprous-chloride/">cupric</a> <a href="/blog/2011/07/03/diy-pcb-etching-collecting-chlorine-gas-read-dont/">chloride</a> <a href="/blog/2011/07/20/diy-pcb-etching-not-electrolyzing-salt-to-make-cupric-chloride/">PCB</a> <a href="/blog/2012/04/12/lsm303dlm-breakout/">etching</a></li>
+    <li>DIY aluminum anodizing*</li>
     <li><a href="/blog/tag/tricopter">Tricopter</a></li>
     <li><a href="/blog/2011/09/10/sawdust-all-over">Woodworking</a></li>
+    <li>Autonomous Crossfire-playing robot*</li>
     <li><a href="http://code.google.com/p/atrias/">Software systems for ATRIAS</a></li>
+    <li>Autonomous maze solver with no code*</li>
 </ul>
 
+<p>* Links forthcoming.</p>
+
 <p>Read my <a href="/blog">blog</a> or visit my <a href="https://github.com/yoos">github page</a>.</p>
+
+<p>I have a plan for beautifying this front page, but there are so many other better projects to be working on.. I promise it'll happen someday.</p>
 </div>
 
 <div id="footer">
-iterativecode
 </div>
 
 <?php include_once("ga.php") ?>
