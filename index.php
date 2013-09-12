9c99c9b27788e0a747f347a2f8827acfc6c89fba
Soo-Hyun Yoo <yoos117@gmail.com>
2013-09-12 11:35:48 -0700

    Add link to projects page.

--- index.php
+++ index.php
@@ -45,23 +45,9 @@ a {
 <div class="block">
 <p style="font-size: 24px; margin: 0;">Hi, I'm Soo-Hyun.</p>
 
-<p>Some of the things I've worked on:</p>
+<p>Check out <a href="/ad_astra">some of my projects</a>,</p>
 
-<ul>
-    <li><a href="/blog/2012/02/27/kiwi-drive-videos/">Kiwi drive <a href="/blog/2012/04/18/l298n-dual-full-bridge-motor-driver/">robot</a></li>
-    <li><a href="/blog/2011/07/20/ceramic-hand">Ceramics</a></li>
-    <li><a href="/blog/2011/06/27/diy-pcb-etching-overview-and-production-of-ferric-chloride/">DIY</a> <a href="/blog/2011/07/02/diy-pcb-etching-electrolyzing-salt-to-produce-cuprous-chloride/">cupric</a> <a href="/blog/2011/07/03/diy-pcb-etching-collecting-chlorine-gas-read-dont/">chloride</a> <a href="/blog/2011/07/20/diy-pcb-etching-not-electrolyzing-salt-to-make-cupric-chloride/">PCB</a> <a href="/blog/2012/04/12/lsm303dlm-breakout/">etching</a></li>
-    <li>DIY aluminum anodizing*</li>
-    <li><a href="/blog/tag/tricopter">Tricopter</a></li>
-    <li><a href="/blog/2011/09/10/sawdust-all-over">Woodworking</a></li>
-    <li>Autonomous Crossfire-playing robot*</li>
-    <li><a href="http://code.google.com/p/atrias/">Software systems for ATRIAS</a></li>
-    <li>Autonomous maze solver with no code*</li>
-</ul>
-
-<p>* Links forthcoming.</p>
-
-<p>Read my <a href="/blog">blog</a> or visit my <a href="https://github.com/yoos">github page</a>.</p>
+<p>Or read my <a href="/blog">blog</a> and visit my <a href="https://github.com/yoos">github page</a>.</p>
 
 <p>I have a plan for beautifying this front page, but there are so many other better projects to be working on.. I promise it'll happen someday.</p>
 </div>
